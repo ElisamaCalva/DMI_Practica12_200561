e0fdedb23c799680e0cd6c67759d7fd4c70d7874
Elisama <84819014+ElisamaCalva@users.noreply.github.com>
2023-10-19 22:05:32 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+// Creado por: Elisama Arturo Calva Moreno
+// Asignatura: Desarrollo Movil Integral
+//Grado: 10   Grupo: "A"
+// Docente: MTI. Marco Antonio Ramirez Hernandez
+
 import 'package:flutter/material.dart';
 import 'package:movieapp_200561/home.dart';
 
