ec7c59bf8b7269d4ab1ee900c2e11f0654f5ef8b
ElisamaCalva <elisamaarturo.calvamoreno@utxicotepec.edu.mx>
2023-10-19 21:50:29 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp_200561/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner:
+        false,
+    home:
+        Home(),
+  ));
+}
